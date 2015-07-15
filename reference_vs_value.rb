a5c9de327ce6e1b075bcdd1ab60db4956d4cb373
Richard <richie.was.here@gmail.com>
2015-07-15 01:22:43 +0000

    2.2 rev

---
+++ reference_vs_value.rb
@@ -0,0 +1,8 @@
+
+arr = [1,2,3,4,5]
+
+def array(x)
+  x.reverse
+end
+
+p array(arr)
---
+++ test.rb
@@ -0,0 +1,11 @@
+
+arr = [1,2,3,4,5]
+
+def array(x)
+  x.reverse
+end
+
+p array(arr)
+
+# it actualy mutates the method without the ! (bang notation) 
+#becuase it returns the given value that was used
